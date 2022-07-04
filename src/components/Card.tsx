fd218de830f7204d7f912cdd2938626aa4f3cd14
bramadl <thenamesbram@gmail.com>
2022-07-04 21:59:09 +0700

    fix: card conditional props and class binding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 
 export const Card: React.FC<{
-  id: string;
-  children: React.ReactNode;
-  header: React.ReactNode | React.ReactFragment;
+	id: string;
+	children?: React.ReactNode;
+	header: React.ReactNode | React.ReactFragment;
 }> = ({ id, header, children }) => {
 	return (
-		<section
-			id={id}
-			className="w-full h-auto rounded-lg bg-dark-200"
-		>
-			<header className="flex items-center justify-between gap-4 p-4 border-b border-white/10">
+		<section id={id} className="w-full h-auto rounded-lg bg-dark-200">
+			<header
+				className={`flex items-center justify-between gap-4 p-4 ${
+					children ? "border-b border-white/10" : ""
+				}`}
+			>
 				{header}
 			</header>
 
-			<div className="p-4">
-				{children}
-			</div>
+			{children && <div className="p-4">{children}</div>}
 		</section>
 	);
 };
