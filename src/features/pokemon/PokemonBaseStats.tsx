50c4d245d0e2bf1690b5b8d75f4952998c0c1822
bramadl <thenamesbram@gmail.com>
2022-07-05 05:43:14 +0700

    chore: styles progress element

--- src/features/pokemon/PokemonBaseStats.tsx
+++ src/features/pokemon/PokemonBaseStats.tsx
@@ -9,17 +9,18 @@ export const PokemonBaseStats: React.FC<{
 	stats: [];
 	weight: number;
 }> = React.memo(({ stats, weight }) => {
-	const statsMapped = stats
-		.map((stat: any) => ({
-			name: stat.stat.name,
-			baseStat: stat.base_stat,
-		}));
-	
+	const statsMapped = stats.map((stat: any) => ({
+		name: stat.stat.name,
+		baseStat: stat.base_stat,
+	}));
+
 	const toKg = Number((weight / 10).toFixed(1));
 	const toLbs = Number((toKg * 2.2).toFixed(1));
 
-	const total = statsMapped.map((stat) => stat.baseStat).reduce((a, b) => a + b, 0);
-	
+	const total = statsMapped
+		.map((stat) => stat.baseStat)
+		.reduce((a, b) => a + b, 0);
+
 	return (
 		<Card
 			id="baseStatsSection"
@@ -36,32 +37,29 @@ export const PokemonBaseStats: React.FC<{
 			}
 		>
 			<ul className="flex flex-col gap-4">
-				{statsMapped
-					.map((stat) => (
-						<li key={stat.name} className="flex gap-8">
-							<div className="w-40">
-								<span className="font-bold capitalize">
-									{stringifySlug(stat.name)}:
-								</span>{" "}
-								<span>{stat.baseStat}</span>
-							</div>
-							<div className="flex-1">
-								<div className="w-full bg-white/10 rounded-full h-3">
-									<div className="bg-green-400 h-full rounded-full" style={{ width: (300 / 100) * stat.baseStat }}></div>
-								</div>
-							</div>
-						</li>
-					))}
-					<li className="flex gap-8">
+				{statsMapped.map((stat) => (
+					<li key={stat.name} className="flex gap-8">
 						<div className="w-40">
 							<span className="font-bold capitalize">
-								Weight:
-							</span>
+								{stringifySlug(stat.name)}:
+							</span>{" "}
+							<span>{stat.baseStat}</span>
 						</div>
 						<div className="flex-1">
-							<span>{toKg} kg or {toLbs} lbs</span>
+							<progress className="w-full" value={stat.baseStat} max={256} />
 						</div>
 					</li>
+				))}
+				<li className="flex gap-8">
+					<div className="w-40">
+						<span className="font-bold capitalize">Weight:</span>
+					</div>
+					<div className="flex-1">
+						<span>
+							{toKg} kg or {toLbs} lbs
+						</span>
+					</div>
+				</li>
 			</ul>
 		</Card>
 	);
