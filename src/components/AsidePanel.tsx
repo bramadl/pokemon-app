3c42e20fcadf15037650e58544abcf07f1a3e27e
Bram Adl <thenamesbram@gmail.com>
2022-07-06 22:05:00 +0700

    chore: update aside width on large and 2xl screen

--- src/components/AsidePanel.tsx
+++ src/components/AsidePanel.tsx
@@ -6,7 +6,7 @@ export const AsidePanel: React.FC<{
 	headerPanel: React.ReactNode;
 }> = ({ children, headerPanel }) => {
 	return (
-		<aside className="w-full md:w-72 xl:w-[420px] h-screen flex flex-col justify-between bg-dark-200 md:border-r border-white/10 overflow-hidden">
+		<aside className="w-full md:w-72 lg:w-96 2xl:w-[420px] h-screen flex flex-col justify-between bg-dark-200 md:border-r border-white/10 overflow-hidden">
 			<header className="flex items-center justify-between gap-6 py-4 px-4 xl:px-8 border-b border-white/10">
 				{headerPanel}
 			</header>
